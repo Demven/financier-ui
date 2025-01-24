a2ce5df6643ac3b4369457ca7fc4103f6130cf83
dsalnikov <dementorvinegar@gmail.com>
2025-01-24 01:25:53 -0500

    v.2.6.1 Updated app.config.js with version

--- app.config.js
+++ app.config.js
@@ -1,46 +1,44 @@
 export default {
   API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL,
-  "expo": {
-    "name": "financier-ui",
-    "slug": "financier-ui",
-    "scheme": "financier-ui",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "bundleIdentifier": "com.demven.financierui",
-      "icon": "./public/images/favicons/apple-touch-icon.png",
-      "runtimeVersion": "1.0.0"
+  expo: {
+    name: "financier-ui",
+    slug: "financier-ui",
+    scheme: "financier-ui",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.demven.financierui",
+      icon: "./public/images/favicons/apple-touch-icon.png",
+      runtimeVersion: "1.0.0",
+      version: "2.6.0",
     },
-    "web": {
-      "bundler": "metro",
-      "favicon": "./public/images/favicons/favicon-32x32.png"
+    web: {
+      bundler: "metro",
+      favicon: "./public/images/favicons/favicon-32x32.png"
     },
-    "splash": {
-      "image": "./assets/images/splash-screen.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#FFFFFF"
+    splash: {
+      image: "./assets/images/splash-screen.png",
+      resizeMode: "contain",
+      backgroundColor: "#FFFFFF"
     },
-    "plugins": [
+    plugins: [
       "expo-router"
     ],
-    "platforms": [
+    platforms: [
       "ios",
       "web"
     ],
-    "android": {
-      "package": "com.demven.financierui"
-    },
-    "extra": {
-      "router": {
-        "origin": false
+    extra: {
+      router: {
+        origin: false
       },
-      "eas": {
-        "projectId": "bc096fe4-114c-429b-90b9-983b2e2555b3"
+      eas: {
+        projectId: "bc096fe4-114c-429b-90b9-983b2e2555b3"
       }
     },
-    "updates": {
-      "url": "https://u.expo.dev/bc096fe4-114c-429b-90b9-983b2e2555b3"
+    updates: {
+      url: "https://u.expo.dev/bc096fe4-114c-429b-90b9-983b2e2555b3"
     },
   }
 };
